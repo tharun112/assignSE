0e19bed3ce6318f103014137c61760e1261a6656
Tharun Reddy <61675167+tharun112@users.noreply.github.com>
2023-12-04 22:01:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    loadAnimeList();
+});
+
+function loadAnimeList() {
+    // Placeholder for loading anime list
+    // In real scenario, you would fetch this from a backend or API
+    const animeList = document.getElementById('anime-list');
+    animeList.innerHTML = '<p>List of Anime Shows</p>';
+    // Add more dynamic content here
+}
