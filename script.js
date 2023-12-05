f3cbfaca328e34a5efb6e134c4f0dab1b081ff2a
Tharun Reddy <61675167+tharun112@users.noreply.github.com>
2023-12-04 23:23:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function loadAnimeList() {
     animeList.innerHTML = '<p>List of Anime Shows</p>';
     // Add more dynamic content here
 }
+
+function loadFeaturedAnime() {
+    // Add content for featured anime
+    const featuredAnime = document.getElementById('featured-anime');
+    featuredAnime.innerHTML = '<p>Featured Anime Titles</p>';
+    // Populate with actual data in a real scenario
+}
+
+function loadAnimeCharacters() {
+    // Add content for anime character showcase
+    const characterShowcase = document.getElementById('anime-character-showcase');
+    characterShowcase.innerHTML = '<p>Popular Anime Characters</p>';
+    // Populate with actual data in a real scenario
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadAnimeList();
+    loadFeaturedAnime();
+    loadAnimeCharacters();
+});
